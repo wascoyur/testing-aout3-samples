542752b2e5e9d577e29e182e7d4712a4aa384141
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-05-22 19:04:30 +0500

    ch2 | mix rules

--- ch2-first-test/__tests__/password-verifier-beforeEatch.spec.js
+++ ch2-first-test/__tests__/password-verifier-beforeEatch.spec.js
@@ -1,5 +1,7 @@
 import { PasswordVerifier1 } from "../password-verifier1";
 
+const templateFakeRule = { passed: true, reason: "" };
+
 describe("PasswordVerifier", () => {
   let verifer;
   beforeEach(() => (verifer = new PasswordVerifier1()));
@@ -10,11 +12,64 @@ describe("PasswordVerifier", () => {
       verifer.addRule(fakeRule);
       errors = verifer.verify("any rule");
     });
+
     it("сообщение ошибки на основе rule.reason", () => {
       expect(errors[0]).toContain("фальшивая причина");
     });
+
     it("only one error", () => {
       expect(errors.length).toBe(1);
+    });
+  });
+
+  describe("передача реальных правил", () => {
+    let fakeRules, errors;
+    beforeEach(() => {
+      fakeRules = (input) => ({ passed: true, reason: "" });
+      verifer.addRule(fakeRules);
+      errors = verifer.verify("any value");
+    });
+    it("has no Errors", () => {
+      expect(errors.length).toBe(0);
+    });
+  });
+
+  describe("with a passing rule", () => {
+    let fakeRules, errors;
+    beforeEach(() => {
+      fakeRules = (input) => templateFakeRule;
+      verifer.addRule(fakeRules);
+      errors = verifer.verify("any value");
+    });
+
+    it("has no errors", () => {
+      expect(errors.length).toBe(0);
+    });
+  });
+
+  describe("with a failing and passing rules", () => {
+    let fakeRulePass, fakeRuleFail, errors;
+    beforeEach(() => {
+      fakeRuleFail = (input) => ({
+        ...templateFakeRule,
+        reason: "fake succsess",
+      });
+      fakeRulePass = (input) => ({
+        ...templateFakeRule,
+        passed: false,
+        reason: "fake reason",
+      });
+      verifer.addRule(fakeRulePass);
+      verifer.addRule(fakeRuleFail);
+      errors = verifer.verify("any value");
+    });
+
+    it("has one error", () => {
+      expect(errors.length).toBe(1);
+    });
+
+    it("error text belongs to failed rule", () => {
+      expect(errors[0]).toContain("fake reason");
     });
   });
 });
